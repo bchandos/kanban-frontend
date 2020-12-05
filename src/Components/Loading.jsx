aa1db4327eda10553d3f51056b2b2cd5710e99ce
Bill <bill.chandos@gmail.com>
2020-12-05 19:00:29 +0000

    animated loading screen

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -4,9 +4,9 @@ class Loading extends React.Component {
     
     render() {
         return (
-            <div className="loading-canvas w3-display-container">
-                <div className="w3-card w3-display-middle w3-padding-large">
-                    <h1>
+            <div className="loading-canvas w3-display-container w3-light-gray">
+                <div className="w3-display-middle w3-padding-large">
+                    <h1 className="w3-animate-fading">
                         Loading...
                     </h1>
                 </div>
